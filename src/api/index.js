05bc076e009c201b30130787376b177d5284930f
Daniel <20521181@gm.uit.edu.vn>
2022-12-04 15:31:26 +0700

    Update new api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://wda-01--cozi-server.herokuapp.com/' }); 
+const API = axios.create({
+  baseURL: 'https://wda01-cozi-server.onrender.com/',
+});
 
-API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
-    return req;
+API.interceptors.request.use(req => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`;
+  }
+  return req;
 });
 
 export const fetchLetters = () => API.get('/letters');
-export const createLetter = (newLetter) => API.post('/letters', newLetter);
-export const updateLetter = (id, updatedLetter) => API.patch(`/letters/${id}`, updatedLetter);
-export const deleteLetter = (id) => API.delete(`/letters/${id}`);
+export const createLetter = newLetter => API.post('/letters', newLetter);
+export const updateLetter = (id, updatedLetter) =>
+  API.patch(`/letters/${id}`, updatedLetter);
+export const deleteLetter = id => API.delete(`/letters/${id}`);
 
-export const signIn = (formData) => API.post('/users/signIn', formData);
-export const signUp = (formData) => API.post('/users/signUp', formData);
+export const signIn = formData => API.post('/users/signIn', formData);
+export const signUp = formData => API.post('/users/signUp', formData);
